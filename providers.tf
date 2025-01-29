ab53573e2772846235bb4afe617bd7774f6a6ddf
Guilherme Alcântara <gui.absilva@outlook.com>
2025-01-29 16:19:44 -0300

    Configure S3 terraform state

--- providers.tf
+++ providers.tf
@@ -5,6 +5,20 @@ terraform {
       version = "5.84.0"
     }
   }
+  backend "s3" {
+    bucket  = "benfica-state-bucket-tf"
+    region  = "sa-east-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
 }
 
 provider "aws" {}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  default     = "benfica-state-bucket-tf"
+  description = "Bucket com o estado"
+}
